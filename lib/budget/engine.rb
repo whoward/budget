aa920ac33f4e1e79c0a0aab3593af97ce1e2f4db
William Howard <whoward.tke@gmail.com>
2015-12-03 23:19:23 -0500

    make it work when used externally

--- lib/budget/engine.rb
+++ lib/budget/engine.rb
@@ -3,11 +3,23 @@ module Budget
   class Engine < ::Rails::Engine
     isolate_namespace Budget
 
+    rake_tasks do
+      load File.join(root, 'lib', 'tasks', 'import.rake')
+    end
+
     initializer 'budget.seeds.ensure' do
+      # don't run this initializer unless the migrations have been run
+      next unless Budget::Category.table_exists?
+
+      # ensure teh category roots exist and are loaded
       %i(income expense transfers transfer_from transfer_to).each do |fixture|
         Category.public_send(fixture) ||
-          fail("unable to load category: #{fixture}, run the seeds for the budget engine")
+          Rails.logger.error("unable to load category: #{fixture}, run the seeds for the budget engine")
       end
+    end
+
+    initializer 'budget.assets.precompile' do |app|
+      app.config.assets.precompile += %w(budget/application.css budget/application.js)
     end
   end
 end
---
+++ lib/generators/budget/install/install_generator.rb
@@ -0,0 +1,20 @@
+module Budget
+  class InstallGenerator < Rails::Generators::Base
+    source_root File.join(__dir__, 'templates')
+
+    def install_migrations
+      say_status :copying, 'migrations'
+      silence_stream(STDOUT) do
+        silence_warnings { rake 'budget:install:migrations' }
+      end
+    end
+
+    def install_route
+      route "mount Budget::Engine => '/'"
+    end
+
+    def include_seed_data
+      append_file 'db/seeds.rb', "\nBudget::Engine.load_seed"
+    end
+  end
+end
