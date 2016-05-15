3c1403740480987a0dfc91dfd50876f118f965b0
Will Howard <whoward.tke@gmail.com>
2016-05-15 10:10:13 -0400

    add depth attribute to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 
-income = Budget::Category.find_or_create_by(name: 'Income')
-expense = Budget::Category.find_or_create_by(name: 'Expense')
-transfers = Budget::Category.find_or_create_by(name: 'Transfers')
+income = Budget::Category.find_or_create_by(name: 'Income', depth: 1)
+expense = Budget::Category.find_or_create_by(name: 'Expense', depth: 1)
+transfers = Budget::Category.find_or_create_by(name: 'Transfers', depth: 1)
 
-Budget::Category.find_or_create_by(name: 'Transfer From', parent_id: transfers.id)
-Budget::Category.find_or_create_by(name: 'Transfer To', parent_id: transfers.id)
+Budget::Category.find_or_create_by(name: 'Transfer From', parent_id: transfers.id, depth: 2)
+Budget::Category.find_or_create_by(name: 'Transfer To', parent_id: transfers.id, depth: 2)
